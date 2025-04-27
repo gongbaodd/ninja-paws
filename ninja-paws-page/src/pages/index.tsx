4457816567e261e223d223a92bc62585352abb87
gongbaodd@ProArt <gongbaodd@qq.com>
2025-04-27 15:04:04 +0300

    init page

---
+++ ninja-paws-page/src/pages/index.tsx
@@ -0,0 +1,23 @@
+export default async function HomePage() {
+  const data = await getData();
+
+  return (
+    <div>
+      <title>{data.title}</title>
+    </div>
+  );
+}
+
+const getData = async () => {
+  const data = {
+    title: 'Ninja Paws',
+  };
+
+  return data;
+};
+
+export const getConfig = async () => {
+  return {
+    render: 'static',
+  } as const;
+};
